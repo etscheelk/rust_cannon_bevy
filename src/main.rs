f0f84c35b5c668b37e1b808c1ad3fb9ed6b4be40
Ethan Scheelk <41307169+etscheelk@users.noreply.github.com>
2025-05-05 22:15:25 -0500

    Inspected alternative queries, tested camera and window settings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiContextPass};
 
+// use bevy::bevy_window::PrimaryWindow;
+
 fn main() {
     App::new()
     .add_plugins((DefaultPlugins, HelloPlugin, EguiPlugin { enable_multipass_for_primary_context: true }))
@@ -8,7 +10,7 @@ fn main() {
     .run();
 }
 
-fn ui_example_system(mut contexts: EguiContexts)
+fn ui_example_system(mut contexts: EguiContexts, query: Query<&Name, With<Person>>)
 {
     egui::Window::new("Hello").show(
         contexts.ctx_mut(), 
@@ -20,6 +22,10 @@ fn ui_example_system(mut contexts: EguiContexts)
                 println!("Button clicked!");
             }
             ui.label("World!");
+            for name in &query
+            {
+                ui.label(format!("Hello, {}!", name.0));
+            }
         }
     );
 }
@@ -33,7 +39,38 @@ impl Plugin for HelloPlugin
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, (setup, add_people, hello_world));
         app.add_systems(Update, (update_people, greet_people).chain());
+        app.add_systems(Update, window_draw);
     }
+}
+
+fn window_draw(
+    mut commands: Commands,
+    mut primary_window: Query<&mut Window>,
+    mut cam: Single<&mut Camera2d>
+) -> ()
+{
+    let Ok(mut window) = primary_window.single_mut() else { return; };
+    let window = &mut *window;
+    
+    let pos = window.cursor_position();
+
+    if let Some(pos) = pos
+    {
+        println!("Cursor position: {:?}", pos);
+    }
+    else
+    {
+        println!("Cursor position: None");
+    }
+
+    window.title = String::from("New Window Title");
+    window.transparent = true;
+
+    // window.set_cursor_visible(false);
+    // window.set_cursor_grab_mode(CursorGrabMode::Confined);
+    // window.set_cursor_grab_mode(CursorGrabMode::Locked);
+    // window.set_cursor_position(Vec2::new(0.0, 0.0));
+    // cam.set_position(Vec3::new(0.0, 0.0, 0.0));
 }
 
 fn setup(mut commands: Commands)
@@ -53,6 +90,13 @@ struct Person;
 #[derive(Component, Debug, Clone, Default)]
 struct Name(String);
 
+#[derive(Bundle)]
+struct PersonBundle
+{
+    person: Person,
+    name: Name,
+}
+
 #[derive(Resource)]
 struct GreetTimer(Timer);
 
@@ -61,13 +105,19 @@ fn add_people(mut commands: Commands)
     commands.spawn((Person, Name(String::from("Elaina Proctor"))));
     commands.spawn((Person, Name(String::from("Renzo Humo"))));
     commands.spawn((Person, Name(String::from("Zayna Hieves"))));
+
+    // commands.spawn(PersonBundle {
+    //     person: Person,
+    //     name: Name(String::from("Elaina Proctor")),
+    // });
 }
 
 fn update_people(mut query: Query<&mut Name, With<Person>>)
 {
     for mut name in &mut query
     {
-        if name.0 == "Elaina Proctor" {
+        if name.0 == "Elaina Proctor" 
+        {
             name.0 = String::from("Elaina Proctor (Updated)");
             break;
         }
